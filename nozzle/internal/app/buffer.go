28c0832f666501ef49fbb202bf38ac3a86a62218
Brady Love <blove@pivotal.io>
2017-12-07 20:08:34 -0700

    Add go docs for buffer in nozzle

--- nozzle/internal/app/buffer.go
+++ nozzle/internal/app/buffer.go
@@ -7,10 +7,12 @@ import (
 	"github.com/cloudfoundry/sonde-go/events"
 )
 
+// Buffer is a simple wrapper around the go-diode for the events.Envelope type.
 type Buffer struct {
 	d *diodes.Poller
 }
 
+// NewBuffer initializes and returns with given size.
 func NewBuffer(size int) *Buffer {
 	d := &Buffer{}
 
@@ -19,10 +21,14 @@ func NewBuffer(size int) *Buffer {
 	return d
 }
 
+// Set adds an envelope to the buffer.
 func (d *Buffer) Set(e *events.Envelope) {
 	d.d.Set(diodes.GenericDataType(e))
 }
 
+// TryNext reads and returns the next envelope. This method will not block, if
+// there is no envelope to read it will return nil for the envelope and a boolean
+// of false.
 func (d *Buffer) TryNext() (*events.Envelope, bool) {
 	e, ok := d.d.TryNext()
 	if !ok {
@@ -32,12 +38,16 @@ func (d *Buffer) TryNext() (*events.Envelope, bool) {
 	return (*events.Envelope)(e), true
 }
 
+// Next reads and returns the next envelope. This method will block until an
+// envelope is available.
 func (d *Buffer) Next() *events.Envelope {
 	e := d.d.Next()
 
 	return (*events.Envelope)(e)
 }
 
+// Alert is used by the internal diode. When envelopes are dropped we simply log
+// a message noting how many envelopes were dropped.
 func (d *Buffer) Alert(missed int) {
 	log.Printf("dropped %d envelopes", missed)
 }
