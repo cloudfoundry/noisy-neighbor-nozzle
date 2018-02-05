ed9f4cba71d48fba17e8266d1c335775245a1dc4
Johanna Smith <josmith@pivotal.io>
2018-02-05 10:00:38 -0700

    Add scriptable flags to deploy script
    [#154613945]
    
    Signed-off-by: Travis Patterson <tpatterson@pivotal.io>

--- cmd/deployer/main.go
+++ cmd/deployer/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -19,8 +20,12 @@ const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var logger = log.New(os.Stderr, "", 0)
 
 func main() {
-	in := inputFromUser()
-	manifestPath := writeManifest(in)
+	var in input
+	userPrompts, in := parseFlags()
+	if userPrompts {
+		in = inputFromUser()
+	}
+	manifestPath := WriteManifest(in)
 
 	execute("push noisy-neighbor-nozzle", "cf", "push", "-f", manifestPath, "--no-start")
 	execute("set subscription ID", "cf", "set-env", in.NozzleAppName, "SUBSCRIPTION_ID", randString(64))
@@ -68,6 +73,87 @@ type input struct {
 	ClientID           string
 	ClientSecret       string
 	SkipCertVerify     bool
+}
+
+func WriteManifest(in input) string {
+	manifest, err := ioutil.ReadFile("manifest_template.yml")
+	if err != nil {
+		logger.Fatalf("Failed to read manifest template: %s", err)
+	}
+
+	for k, v := range in.toEnv() {
+		manifest = bytes.Replace(manifest, []byte("$"+k), []byte(v), -1)
+	}
+
+	outfile, err := ioutil.TempFile("", "manifest-")
+	if err != nil {
+		logger.Fatalf("Failed to create temporary file: %s", err)
+	}
+	defer outfile.Close()
+
+	_, err = outfile.Write(manifest)
+	if err != nil {
+		logger.Fatalf("Failed writing to temporary manifest: %s", err)
+	}
+
+	return outfile.Name()
+}
+
+func parseFlags() (bool, input) {
+	promptInput := flag.Bool(
+		"interactive",
+		false,
+		"True if you want user prompts to configure app setup. Otherwise, will assume all input via flags.",
+	)
+	sysDomain := flag.String("system-domain", "", "Your system domain")
+	appDomain := flag.String("app-domain", *sysDomain, "Your app domain")
+	nozzleAppName := flag.String("nozzle-app-name", "nn-nozzle", "")
+	accAppName := flag.String("accumulator-app-name", "nn-accumulator", "")
+	instances := flag.Uint(
+		"nozzle-instances",
+		4,
+		"The number of noisy neighbor nozzles to deploy",
+	)
+	uaa := flag.String(
+		"uaa-addr",
+		"https://uaa.<system-domain>",
+		"The address of the UAA server in Cloud Foundry. This can be discovered by looking at the token_endpoint field from cf curl v2/info.",
+	)
+	loggrAddr := flag.String(
+		"loggregator-addr",
+		"wss://doppler.<system-domain>:443",
+		"The address of the Cloud Foundry deployed Loggregator. This can be discovered by looking at the doppler_logging_endpoint field from cf curl v2/info.",
+	)
+	clientID := flag.String(
+		"client-id",
+		"",
+		"The UAA client ID with the correct authorities.",
+	)
+	secret := flag.String(
+		"client-secret",
+		"",
+		"The corresponding secret for the given client ID.",
+	)
+	skipCertVerify := flag.Bool(
+		"skip-cert-verify",
+		false,
+		"Yes if the Cloud Foundry is using self signed certs.",
+	)
+
+	flag.Parse()
+
+	return *promptInput, input{
+		SystemDomain:       *sysDomain,
+		AppDomain:          *appDomain,
+		NozzleAppName:      *nozzleAppName,
+		AccumulatorAppName: *accAppName,
+		NozzleInstances:    *instances,
+		UAAAddr:            *uaa,
+		LoggregatorAddr:    *loggrAddr,
+		ClientID:           *clientID,
+		ClientSecret:       *secret,
+		SkipCertVerify:     *skipCertVerify,
+	}
 }
 
 func inputFromUser() input {
@@ -173,28 +259,5 @@ func unsignedInteger(val interface{}) error {
 	}
 	_, err := strconv.ParseUint(vs, 10, 64)
 	return err
-}
 
-func writeManifest(in input) string {
-	manifest, err := ioutil.ReadFile("manifest_template.yml")
-	if err != nil {
-		logger.Fatalf("Failed to read manifest template: %s", err)
-	}
-
-	for k, v := range in.toEnv() {
-		manifest = bytes.Replace(manifest, []byte("$"+k), []byte(v), -1)
-	}
-
-	outfile, err := ioutil.TempFile("", "manifest-")
-	if err != nil {
-		logger.Fatalf("Failed to create temporary file: %s", err)
-	}
-	defer outfile.Close()
-
-	_, err = outfile.Write(manifest)
-	if err != nil {
-		logger.Fatalf("Failed writing to temporary manifest: %s", err)
-	}
-
-	return outfile.Name()
 }
